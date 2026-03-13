3b38c421715e31cf0ff9325ecae731d273fc3607
ebenezermamidi-bsu <ebenezer.mamidi@bsu.edu>
2026-03-13 01:13:10 +0000

    misc: comments cleanup

--- LibraryManagementSystem/LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         ConsoleUI theUI = new ConsoleUI();
-        theUI.SeedData(); //remove when I get actual data
+        theUI.SeedData();
         theUI.Show();
     }
 }
